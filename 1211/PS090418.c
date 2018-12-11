0b37fe876cc8e830db0811c899204c0ac676418c
05050146 <37133592+05050146@users.noreply.github.com>
2018-12-11 11:12:20 +0800

    Add files via upload

---
+++ 1211/PS090418.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void afunc(char a[],char b[],char c[]);
+int main(void) 
+{          
+    char a[80],b[80],c[80]; 
+
+    scanf("%s",a); 
+    scanf("%s",b); 
+    afunc(a,b,c); 
+    printf("[%s]\n",c); 
+     
+    return 0; 
+} 
+void afunc(char a[],char b[],char c[])
+{
+	int i;
+	for(i=0;a[i]!=0;i++)
+	{	
+		if(a[i]=='1' && b[i]=='1')
+		c[i]='1';
+		else
+		c[i]='0';
+	}
+	
+	return c;
+}
